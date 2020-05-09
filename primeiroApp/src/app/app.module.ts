51d6c1035094604e476d82c3182890dd5b7af140
Raniel Santos <ranielsnts@hotmail.com>
2020-05-09 01:04:44 +0000

    Criando contar module

--- primeiroApp/src/app/app.module.ts
+++ primeiroApp/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {ContarModule} from './contar'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ContarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
